1476b79d93562354059d599f4cfc5923927fbf96
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2024-07-11 11:46:11 -0600

    - Parser extension function moved into "Data+Extensions." - Update access control of "parseForBox" & "parseForFullBox" to public.

---
+++ Sources/MP4Parser/Data+Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kiyoshi Nagahama on 7/11/24.
+//
+
+import Foundation
+
+extension Data {
+    
+    public func parseForMP4Data() async throws -> QTBox {
+        
+        let rootBox = QTRootBox(data: self, location: 0..<UInt32(self.count), type: .root)
+        await rootBox.process()
+        return rootBox
+    }
+    
+    public func parseForBox(offset: UInt32 = 0) throws -> QTBox {
+        
+        // Getting size
+        let size: UInt32 = self[offset..<4+offset].QTUtilConvert(type: UInt32.self)
+        
+        // Getting boxType
+        guard let boxTypeString = String(data: self[4+offset..<8+offset], encoding: .utf8), let type = QTBoxType(rawValue: boxTypeString) else {
+            throw QTBoxError.unableToGetBoxType
+        }
+        
+        let box = QTBox(data: self, location: offset..<offset + size, type: type)
+        
+        return box
+    }
+    
+    public func parseForFullBox(offset: UInt32 = 0) throws -> QTFullBox {
+        
+        // Getting size
+        let size: UInt32 = self[offset..<4+offset].QTUtilConvert(type: UInt32.self)
+        
+        // Getting boxType
+        guard let boxTypeString = String(data: self[4+offset..<8+offset], encoding: .utf8), let type = QTBoxType(rawValue: boxTypeString) else {
+            throw QTBoxError.unableToGetBoxType
+        }
+        
+        let fullbox = QTFullBox(data: self, location: offset..<offset + size, type: type)
+        
+        return fullbox
+    }
+}
